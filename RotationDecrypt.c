93079f1c1428e8984fd15db40ef781ecd982a947
Ryan Stutchbury <ryan.stutch@gmail.com>
2019-04-10 00:39:33 +0000

    yikes

--- RotationDecrypt.c
+++ RotationDecrypt.c
@@ -1,24 +1,326 @@
 #include <stdio.h>
 int main()
 { int i=0;
-int key=9;
-char str[500]={"dniels gezAGVa HBJNMZzy"};
+int key=26;
+char str[500]={"aA"};
     for(i=0; str[i]!='\0' ; i++){
-        
-        if(str[i] != 32){
-        
-            if((str[i]+key)<(92+key)){
-              if (str[i]+key>=91){
-                str[i]=str[i]-26;
+        switch(str[i]){
+            case(32):
+            break;
+            case(65):
+                if((str[i]-key)<=(64)){
+                str[i]=str[i]+26;
+        }
+        str[i] =str[i]-key;
+            break;
+            case(66):
+                if((str[i]-key)<=(64)){
+                str[i]=str[i]+26;
+        }
+        str[i] =str[i]-key;
+            break;
+            case(67):
+                if((str[i]-key)<=(96)){
+                    str[i]=str[i]+26;
+                }
+                str[i] =str[i]-key;
+                break;
+            case(68):
+                if((str[i]-key)<=(64)){
+                str[i]=str[i]+26;
+        }
+        str[i] =str[i]-key;
+            break;
+            case(69):
+                if((str[i]-key)<=(64)){
+                str[i]=str[i]+26;
+        }
+        str[i] =str[i]-key;
+            break;
+            case(70):
+                if((str[i]-key)<=(64)){
+                str[i]=str[i]+26;
+        }
+        str[i] =str[i]-key;
+            break;
+            case(71):
+                if((str[i]-key)<=(64)){
+                str[i]=str[i]+26;
+        }
+        str[i] =str[i]-key;
+            break;
+            case(72):
+                if((str[i]-key)<=(64)){
+                str[i]=str[i]+26;
+        }
+        str[i] =str[i]-key;
+            break;
+            case(73):
+                if((str[i]-key)<=(64)){
+                str[i]=str[i]+26;
+        }
+        str[i] =str[i]-key;
+            break;
+            case(74):
+                if((str[i]-key)<=(64)){
+                str[i]=str[i]+26;
+        }
+        str[i] =str[i]-key;
+            break;
+            case(75):
+                if((str[i]-key)<=(64)){
+                str[i]=str[i]+26;
+        }
+        str[i] =str[i]-key;
+            break;
+            case(76):
+                if((str[i]-key)<=(64)){
+                str[i]=str[i]+26;
+        }
+        str[i] =str[i]-key;
+            break;
+            case(77):
+                if((str[i]-key)<=(64)){
+                str[i]=str[i]+26;
+        }
+        str[i] =str[i]-key;
+            break;
+            case(78):
+                if((str[i]-key)<=(64)){
+                str[i]=str[i]+26;
+        }
+        str[i] =str[i]-key;
+            break;
+            case(79):
+                if((str[i]-key)<=(64)){
+                str[i]=str[i]+26;
+        }
+        str[i] =str[i]-key;
+            break;
+            case(80):
+                if((str[i]-key)<=(64)){
+                str[i]=str[i]+26;
+        }
+        str[i] =str[i]-key;
+            break;
+            case(81):
+                if((str[i]-key)<=(64)){
+                str[i]=str[i]+26;
+        }
+        str[i] =str[i]-key;
+            break;
+            case(82):
+                if((str[i]-key)<=(64)){
+                str[i]=str[i]+26;
+        }
+        str[i] =str[i]-key;
+            break;
+            case(83):
+                if((str[i]-key)<=(64)){
+                str[i]=str[i]+26;
+        }
+        str[i] =str[i]-key;
+            break;
+            case(84):
+                if((str[i]-key)<=(64)){
+                str[i]=str[i]+26;
+        }
+        str[i] =str[i]-key;
+            break;
+            case(85):
+                if((str[i]-key)<=(64)){
+                str[i]=str[i]+26;
+        }
+        str[i] =str[i]-key;
+            break;
+            case(86):
+                if((str[i]-key)<=(64)){
+                str[i]=str[i]+26;
+        }
+        str[i] =str[i]-key;
+            break;
+            case(87):
+                if((str[i]-key)<=(64)){
+                str[i]=str[i]+26;
+        }
+        str[i] =str[i]-key;
+            break;
+            case(88):
+                if((str[i]-key)<=(64)){
+                str[i]=str[i]+26;
+        }
+        str[i] =str[i]-key;
+            break;
+            case(89):
+                if((str[i]-key)<=(64)){
+                str[i]=str[i]+26;
+        }
+        str[i] =str[i]-key;
+            break;
+            case(90):
+                if((str[i]-key)<=(64)){
+                str[i]=str[i]+26;
+        }
+        str[i] =str[i]-key;
+            break;
+            case(97):
+                if((str[i]-key)<=(96)){
+                str[i]=str[i]+26;
+        }
+        str[i] =str[i]-key;
+            break;
+            case(98):
+                if((str[i]-key)<=(96)){
+                str[i]=str[i]+26;
+        }
+        str[i] =str[i]-key;
+            break;
+             case(99):
+                if((str[i]-key)<=(96)){
+                str[i]=str[i]+26;
+        }
+        str[i] =str[i]-key;
+            break;
+             case(100):
+                if((str[i]-key)<=(96)){
+                str[i]=str[i]+26;
+        }
+        str[i] =str[i]-key;
+            break;
+             case(101):
+                if((str[i]-key)<=(96)){
+                str[i]=str[i]+26;
+        }
+        str[i] =str[i]-key;
+            break;
+             case(102):
+                if((str[i]-key)<=(96)){
+                str[i]=str[i]+26;
+        }
+        str[i] =str[i]-key;
+            break;
+             case(103):
+                if((str[i]-key)<=(96)){
+                str[i]=str[i]+26;
+        }
+        str[i] =str[i]-key;
+            break;
+             case(104):
+                if((str[i]-key)<=(96)){
+                str[i]=str[i]+26;
+        }
+        str[i] =str[i]-key;
+            break;
+             case(105):
+                if((str[i]-key)<=(96)){
+                str[i]=str[i]+26;
+        }
+        str[i] =str[i]-key;
+            break;
+             case(106):
+                if((str[i]-key)<=(96)){
+                str[i]=str[i]+26;
+        }
+        str[i] =str[i]-key;
+            break;
+             case(107):
+                if((str[i]-key)<=(96)){
+                str[i]=str[i]+26;
+        }
+        str[i] =str[i]-key;
+            break;
+             case(108):
+                if((str[i]-key)<=(96)){
+                str[i]=str[i]+26;
+        }
+        str[i] =str[i]-key;
+            break;
+             case(109):
+                if((str[i]-key)<=(96)){
+                str[i]=str[i]+26;
+        }
+        str[i] =str[i]-key;
+            break;
+             case(110):
+                if((str[i]-key)<=(96)){
+                str[i]=str[i]+26;
+        }
+        str[i] =str[i]-key;
+            break;
+             case(111):
+                if((str[i]-key)<=(96)){
+                str[i]=str[i]+26;
+        }
+        str[i] =str[i]-key;
+            break;
+             case(112):
+                if((str[i]-key)<=(96)){
+                str[i]=str[i]+26;
+        }
+        str[i] =str[i]-key;
+            break;
+             case(113):
+                if((str[i]-key)<=(96)){
+                str[i]=str[i]+26;
+        }
+        str[i] =str[i]-key;
+            break;
+             case(114):
+                if((str[i]-key)<=(96)){
+                str[i]=str[i]+26;
+        }
+        str[i] =str[i]-key;
+            break;
+             case(115):
+                if((str[i]-key)<=(96)){
+                str[i]=str[i]+26;
+        }
+        str[i] =str[i]-key;
+            break;
+             case(116):
+                if((str[i]-key)<=(96)){
+                str[i]=str[i]+26;
+        }
+        str[i] =str[i]-key;
+            break;
+             case(117):
+                if((str[i]-key)<=(96)){
+                str[i]=str[i]+26;
+        }
+        str[i] =str[i]-key;
+            break;
+             case(118):
+                if((str[i]-key)<=(96)){
+                str[i]=str[i]+26;
+        }
+        str[i] =str[i]-key;
+            break;
+             case(119):
+                if((str[i]-key)<=(96)){
+                str[i]=str[i]+26;
+        }
+        str[i] =str[i]-key;
+            break;
+             case(120):
+                if((str[i]-key)<=(96)){
+                str[i]=str[i]+26;
+        }
+        str[i] =str[i]-key;
+            break;
+             case(121):
+                if((str[i]-key)<=(96)){
+                str[i]=str[i]+26;
+        }
+        str[i] =str[i]-key;
+            break;
+             case(122):
+                if((str[i]-key)<=(96)){
+                str[i]=str[i]+26;
+        }
+        str[i] =str[i]-key;
+            break;
         }
         }
-            if((str[i]+key)>=(123)){
-            str[i]=str[i]-26;
-        }
-           str[i] =str[i]+key;
-     }   
-    }
-
    printf("%s", str);
     return 0;
 }
